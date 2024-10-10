57d2ad3c00f5fa147d3e8607e30ef8c60bc303ad
vickykasprik <viktoriakasprik05@gmail.com>
2024-10-10 07:35:24 +0000

    zadania1.1

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-(function () {
-  //TODO
-})();
+var ex_1_button = document.getElementById("ex1_button");
+
+ex1_button.onclick = function () {
+  var tabela = [];
+
+  for (var i = 0; i <= 9; i++) {
+    tabela.push(i);
+  }
+
+  ex1_content.innerHTML = tabela.toString();
+};
